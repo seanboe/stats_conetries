e314cbe2834bb3c70522a22e8c7f5a7afcca5123
Sean Boerhout <boeseany@gmail.com>
2023-06-01 19:21:19 -0700

    interpretation generation

--- main.py
+++ main.py
@@ -64,8 +64,29 @@ def plot_data(series_x, series_y, regression_line=True):
     new_df.plot(x=series_x, y='Treg', color='Red', ax=ax)
     plt.legend([series_x, f'{f}'])
 
+  interpret_data(series_x, series_y, slope, rvalue, pvalue, stderr, 0.05)
+
   # plot
   plt.show()
+  return rvalue, pvalue, stderr
+
+def interpret_data(series_x, series_y, slope, rvalue, pvalue, stderr, alpha):
+  output = 'There is a ' 
+  output += ('positive') if slope > 0 else ('negative') 
+  output += ', linear, and '
+  output += 'moderately strong to strong' if rvalue > 0.85 else 'weak'
+  output += f' relationship between {series_x} and {series_y}. '
+  if pvalue < alpha:
+    output += f'Since the p-value is less than {alpha*100}%, reject H0, and there is statistically significant evidence of a linear relationship. '
+  else:
+    output += f'Since the p-value is greater than {alpha*100}%, fail to reject H0, and there is no statistically significant evidence of a linear relationship.'
+  # Interpret R^2
+  output += f'{rvalue**2 * 100}% of the variation in {series_y} is explained by the linear relationship between {series_x} and {series_y}.'
+  output += f'The linear regression has a standard error of {stderr}.'
+
+  # Return a formatted version for a regression table
+
+
 
 
 if __name__ == "__main__":
